f7828dd0f83799ed709af720fb40eeddf1cb2d47
achelh <achelh624@boscdelacoma.cat>
2026-04-27 13:46:39 +0200

    Update Llanterna.java

--- Llanterna.java
+++ Llanterna.java
@@ -6,17 +6,6 @@ public class Llanterna extends Item {
         this.encesa = false;
     }
 
-    public boolean isEncesa() { return encesa; }
-
-    public void usar() {
-        encesa = !encesa;
-        if (encesa) {
-            System.out.println("Has encès la llanterna.");
-        } else {
-            System.out.println("Has apagat la llanterna.");
-        }
-    }
-
     @Override
     public String toString() {
         String estat = encesa ? "encesa" : "apagada";
